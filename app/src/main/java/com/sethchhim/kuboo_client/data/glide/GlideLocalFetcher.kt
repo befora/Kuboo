a8c109e1fad9adc88058b0dd842c5187fcfa9e29
Seth Chhim <sethchhim@gmail.com>
2018-12-22 14:27:34 -0500

    fix force downsize did not apply to local

--- app/src/main/java/com/sethchhim/kuboo_client/data/glide/GlideLocalFetcher.kt
+++ app/src/main/java/com/sethchhim/kuboo_client/data/glide/GlideLocalFetcher.kt
@@ -1,13 +1,19 @@
 package com.sethchhim.kuboo_client.data.glide
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.data.DataFetcher
 import com.sethchhim.kuboo_client.BaseApplication
+import com.sethchhim.kuboo_client.Settings
 import com.sethchhim.kuboo_client.data.ViewModel
 import com.sethchhim.kuboo_client.data.model.GlideLocal
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import javax.inject.Inject
+
 
 class GlideLocalFetcher internal constructor(private val glideLocal: GlideLocal) : DataFetcher<InputStream> {
 
@@ -40,7 +46,7 @@ class GlideLocalFetcher internal constructor(private val glideLocal: GlideLocal)
 
     private fun handleResult(callback: DataFetcher.DataCallback<in InputStream>, result: InputStream?) {
         if (result != null) {
-            inputStream = result
+            inputStream = result.scaleToTargetWidth()
             callback.onDataReady(inputStream)
         } else {
             callback.onLoadFailed(Exception("Failed to load!"))
@@ -59,6 +65,55 @@ class GlideLocalFetcher internal constructor(private val glideLocal: GlideLocal)
             inputStream.close()
         } catch (e: Exception) {
         }
+    }
+
+    private fun InputStream.scaleToTargetWidth(): InputStream {
+        return if (Settings.MAX_PAGE_WIDTH >= Settings.DEFAULT_MAX_PAGE_WIDTH) {
+            this
+        } else {
+            val bitmap = BitmapFactory.decodeStream(this)
+            this.close()
+            bitmap
+                    .resize(Settings.MAX_PAGE_WIDTH, 9999)
+                    .toInputStream()
+        }
+    }
+
+    private fun Bitmap.resize(maxWidth: Int, maxHeight: Int): Bitmap {
+        try {
+            if (maxHeight > 0 && maxWidth > 0) {
+                val ratioBitmap = width.toFloat() / height.toFloat()
+                val ratioMax = maxWidth.toFloat() / maxHeight.toFloat()
+
+                var finalWidth = maxWidth
+                var finalHeight = maxHeight
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (maxHeight.toFloat() * ratioBitmap).toInt()
+                } else {
+                    finalHeight = (maxWidth.toFloat() / ratioBitmap).toInt()
+                }
+                val newBitmap = Bitmap.createScaledBitmap(this, finalWidth, finalHeight, true)
+                this.recycle()
+                return newBitmap
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return this
+    }
+
+    private fun Bitmap.toInputStream(): InputStream {
+        val baos = ByteArrayOutputStream()
+        try {
+            compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            recycle()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val bytes = baos.toByteArray()
+        baos.close()
+        return ByteArrayInputStream(bytes)
+
     }
 
     override fun getDataClass() = InputStream::class.java
