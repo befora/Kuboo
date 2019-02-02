c8da26a82aa9b5434a4b1fc9e03256f7b65f185d
Seth Chhim <sethchhim@gmail.com>
2019-02-02 01:41:47 -0500

    code refactor

--- app/src/main/java/com/sethchhim/kuboo_client/util/DialogUtil.kt
+++ app/src/main/java/com/sethchhim/kuboo_client/util/DialogUtil.kt
@@ -3,12 +3,11 @@ package com.sethchhim.kuboo_client.util
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
-import com.google.android.material.snackbar.Snackbar
-import androidx.core.content.ContextCompat
-import androidx.appcompat.app.AlertDialog
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.sethchhim.kuboo_client.Extensions.guessFilename
 import com.sethchhim.kuboo_client.Extensions.toReadable
 import com.sethchhim.kuboo_client.R
@@ -17,8 +16,8 @@ import com.sethchhim.kuboo_client.Settings.APP_THEME
 import com.sethchhim.kuboo_remote.model.Book
 import com.sethchhim.kuboo_remote.model.Response
 import org.jetbrains.anko.collections.forEachWithIndex
+import org.jetbrains.anko.layoutInflater
 import java.io.File
-
 
 class DialogUtil(val context: Context) {
 
@@ -68,15 +67,15 @@ class DialogUtil(val context: Context) {
 
     internal fun getDialogBookmark(context: Context) = getAlertDialogBuilder(context).apply {
         setTitle(context.getString(R.string.dialog_bookmark_found))
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_bookmark, null))
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_bookmark, null))
     }.create()
 
     internal fun getDialogBookSettings(context: Context) = getAlertDialogBuilder(context).apply {
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_book_settings, null))
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_book_settings, null))
     }.create()
 
     internal fun getDialogChangeLog(context: Context) = getAlertDialogBuilder(context, appTheme = 1).apply {
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_changelog, null))
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_changelog, null))
     }.create()
 
     internal fun getDialogDownloadStart(context: Context, downloadList: List<Book>) = getAlertDialogBuilder(context).apply {
@@ -94,7 +93,7 @@ class DialogUtil(val context: Context) {
         val message = "File Size: $fileSize\nLocation: $filePath\n"
         setTitle(fileName)
         setMessage(message)
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_download_item_settings, null))
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_download_item_settings, null))
         setNeutralButton(context.getString(R.string.dialog_delete)) { dialog, which -> onDialogSelect0.onSelect0() }
     }.create()
 
@@ -111,8 +110,8 @@ class DialogUtil(val context: Context) {
     }.create()
 
     internal fun getDialogForceDownsizing(context: Context) = getAlertDialogBuilder(context).apply {
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_reader_force_downsizing, null))
-        setPositiveButton(context.getString(R.string.dialog_default)) { dialog, which ->  }
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_reader_force_downsizing, null))
+        setPositiveButton(context.getString(R.string.dialog_default)) { dialog, which -> }
     }.create()
 
     internal fun getDialogHomeLayout(context: Context, onDialogSelect2: OnDialogSelect2) = getAlertDialogBuilder(context).apply {
@@ -149,8 +148,8 @@ class DialogUtil(val context: Context) {
     }.create()
 
     internal fun getDialogRecentlyViewedHeightOffset(context: Context) = getAlertDialogBuilder(context).apply {
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_settings_recently_viewed_height_offset, null))
-        setPositiveButton(context.getString(R.string.dialog_default)) { dialog, which ->  }
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_settings_recently_viewed_height_offset, null))
+        setPositiveButton(context.getString(R.string.dialog_default)) { dialog, which -> }
         setMessage(context.getString(R.string.settings_adjust_height_offset_of_the_recently_viewed_row))
 
     }.create()
@@ -174,11 +173,11 @@ class DialogUtil(val context: Context) {
     }.create()
 
     internal fun getDialogTrackingInterval(context: Context) = getAlertDialogBuilder(context).apply {
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_settings_tracking_interval, null))
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_settings_tracking_interval, null))
     }.create()
 
     internal fun getDialogTrackingLimit(context: Context) = getAlertDialogBuilder(context).apply {
-        setView(LayoutInflater.from(context).inflate(R.layout.dialog_layout_settings_tracking_limit, null))
+        setView(context.layoutInflater.inflate(R.layout.dialog_layout_settings_tracking_limit, null))
     }.create()
 
     private fun getAlertDialogBuilder(context: Context, appTheme: Int = APP_THEME): AlertDialog.Builder {
